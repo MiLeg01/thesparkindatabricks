c3eb092e76129e8cb7413f231863bd78729f9dae
MiLeg01 <MiLeg01>
2025-10-02 19:38:44 +0000

    Update Asset bundles

--- Modul 7 - Asset Bundle erstellen.py
+++ Modul 7 - Asset Bundle erstellen.py
@@ -1,17 +1,23 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC # Databricks Bundles Walkthrough
+# MAGIC # Databricks Asset Bundles
 # MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ## 1. Authentifizierung in der Databricks CLI
-# MAGIC
+# MAGIC > 
 # MAGIC ```sh
 # MAGIC databricks auth login --host <workspace-url>
 # MAGIC ```
-# MAGIC
-# MAGIC ---
-# MAGIC
+# MAGIC Username := Mailadresse
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ## 2. Initialize Databricks Bundle
-# MAGIC
+# MAGIC > 
 # MAGIC ```sh
 # MAGIC databricks bundle init
 # MAGIC ```
@@ -23,24 +29,29 @@
 # MAGIC - no sample DLT pipeline
 # MAGIC - no sample Python package
 # MAGIC - **yes** to serverless
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 3. Review Bundlestruktur & src Folder
 # MAGIC
-# MAGIC ---
-# MAGIC
-# MAGIC ## 3. Review der Bundlestruktur & Create `src` Folder
-# MAGIC
-# MAGIC - Review und erkläre die Folderstruktur
-# MAGIC - Erzeuge einen`src/` Ordner und füge .py Files hinzu
-# MAGIC
-# MAGIC ---
-# MAGIC
+# MAGIC - Review der Folderstruktur
+# MAGIC - Erzeuge einen src/ Ordner und füge .py Files hinein die deployed und ausgeführt werden sollen
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ## 4. Review `databricks.yml`
 # MAGIC
-# MAGIC - Review und erkläre das File databricks.yml
+# MAGIC - Review des Files databricks.yml
+# MAGIC - databricks.yml ist das zentrale File in der Databricks Asset Bundle Struktur
 # MAGIC
-# MAGIC ---
-# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ## 5. Erzeuge ein File `variables.yml` in \\resources
-# MAGIC
+# MAGIC > 
 # MAGIC ```yaml
 # MAGIC variables:
 # MAGIC   catalog:
@@ -49,22 +60,13 @@
 # MAGIC   schema:
 # MAGIC     description: Schema name
 # MAGIC     default: default
-# MAGIC   table_name:
-# MAGIC     description: Target table
-# MAGIC     default: -
 # MAGIC ```
-# MAGIC
-# MAGIC ---
-# MAGIC
-# MAGIC ## 6. Erzeuge einen Catalog/Schema in Databricks
-# MAGIC
-# MAGIC - Set up the catalog, if not created yet and fill in the variables.
-# MAGIC - Erzeuge einen Catalog/Schema in Databricks, falls benötigt und fülle die entsprechenden Namen im variables.yml ein
-# MAGIC
-# MAGIC ---
-# MAGIC
-# MAGIC ## 7. Definiere einen Job in `databricks.yml`
-# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 6. Definiere einen Job in `databricks.yml`
+# MAGIC > 
 # MAGIC ```yaml
 # MAGIC resources:
 # MAGIC   jobs:
@@ -73,48 +75,49 @@
 # MAGIC       tasks:
 # MAGIC         - task_key: run_etl
 # MAGIC           spark_python_task:
-# MAGIC             python_file: src/etl_job.py
+# MAGIC             python_file: src/df_operations.py
+# MAGIC           environment_key: Default
+# MAGIC       environments:
+# MAGIC         - environment_key: Default
+# MAGIC           spec:
+# MAGIC             client: "3"
 # MAGIC ```
 # MAGIC
-# MAGIC ## 8. Erstes Deployment
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 7. Bundle validieren, deployen und Run durchführen
 # MAGIC
+# MAGIC **Validieren:**
+# MAGIC ```sh
+# MAGIC databricks bundle validate
+# MAGIC ```
+# MAGIC
+# MAGIC **Deployen:**
 # MAGIC ```sh
 # MAGIC databricks bundle deploy
 # MAGIC ```
 # MAGIC
-# MAGIC ---
-# MAGIC
-# MAGIC ## 9. Erzeuge einen Service Principal
-# MAGIC
-# MAGIC - Weise deinem Konto die Rolle User zu.
-# MAGIC - Gib dem Service-Principal Berechtigungen für den Katalog.
-# MAGIC
-# MAGIC ---
-# MAGIC
-# MAGIC ## 10. Run Job & Staging Environment konfigurieren
-# MAGIC
-# MAGIC ```yaml
-# MAGIC staging:
-# MAGIC   presets:
-# MAGIC     name_prefix: "[staging SP]"
-# MAGIC   workspace:
-# MAGIC     host: https://...
-# MAGIC     root_path: /Workspace/Shared/.bundle/${bundle.name}/${bundle.target}
-# MAGIC   run_as:
-# MAGIC     service_principal_name: tbd
-# MAGIC   permissions:
-# MAGIC     - user_name: tbd
-# MAGIC       level: CAN_MANAGE
-# MAGIC     - group_name: users
-# MAGIC       level: CAN_MANAGE
+# MAGIC **Run:**
+# MAGIC ```sh
+# MAGIC databricks bundle run -t dev etl_job
 # MAGIC ```
 # MAGIC
-# MAGIC - Run a **staging checkout run**.
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 8. Erzeuge einen Service Principal
 # MAGIC
-# MAGIC ---
-# MAGIC
-# MAGIC ## 11. Produktionsumgebung anpassen
-# MAGIC
+# MAGIC - Service Principal in der Admin-Konsole erzeugen
+# MAGIC - Service Principal berechtigen
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ## 9. Produktionsumgebung anpassen
+# MAGIC > 
 # MAGIC ```yaml
 # MAGIC prod:
 # MAGIC   mode: production
@@ -132,10 +135,12 @@
 # MAGIC       level: CAN_MANAGE
 # MAGIC ```
 # MAGIC
-# MAGIC ---
-# MAGIC
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC ## 12. Run Production Deployment
-# MAGIC
+# MAGIC >             
 # MAGIC ```sh
 # MAGIC databricks bundle deploy -t prod
 # MAGIC ```
