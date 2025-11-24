1243be71f7c12dad0f93f4f3f624fa49033656b9
MiLeg01 <michael.legenstein@gmail.com>
2025-11-24 20:58:29 +0000

    timetravel

--- Modul 4 - In- und Output Operationen.py
+++ Modul 4 - In- und Output Operationen.py
@@ -33,34 +33,34 @@ VOLUME_PATH = f"/Volumes/{CATALOG}/{SCHEMA}/taxi_volume/"
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## 4.2. Speicherung im Parquet-Format
+# MAGIC ##4.2. Speichern im JSON Format
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### 4.2.1. Speichern
+# MAGIC ### 4.2.1 Speichern
 
 # COMMAND ----------
 
-# Taxi-Daten als Parquet im Managed Volume speichern
-df_taxi.write.mode("overwrite").parquet(f"{VOLUME_PATH}/taxi_parquet")
+# Taxi-Daten als JSON speichern
+df_taxi.write.mode("overwrite").json(f"{VOLUME_PATH}/taxi_json")
 
-# Lookup-Daten als Parquet speichern
-df_lookup.write.mode("overwrite").parquet(f"{VOLUME_PATH}/lookup_parquet")
+# Lookup-Daten als JSON speichern
+df_lookup.write.mode("overwrite").json(f"{VOLUME_PATH}/lookup_json")
 
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ### 4.2.2. Auslesen
+# MAGIC ###4.2.2. Einlesen
 
 # COMMAND ----------
 
-df_taxi_parquet = spark.read.parquet(f"{VOLUME_PATH}/taxi_parquet")
-df_lookup_parquet = spark.read.parquet(f"{VOLUME_PATH}/lookup_parquet")
+df_taxi_json = spark.read.json(f"{VOLUME_PATH}/taxi_json")
+df_lookup_json = spark.read.json(f"{VOLUME_PATH}/lookup_json")
 
-df_taxi_parquet.show(5)
-df_lookup_parquet.show(5)
+df_taxi_json.show(5)
+df_lookup_json.show(5)
 
 
 # COMMAND ----------
@@ -99,12 +99,47 @@ df_lookup_csv.show(5)
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##4.4. Speichern im Delta Format
+# MAGIC ## 4.4. Speicherung im Parquet-Format
 
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###4.4.1. Speichern
+# MAGIC ### 4.4.1. Speichern
+
+# COMMAND ----------
+
+# Taxi-Daten als Parquet im Managed Volume speichern
+df_taxi.write.mode("overwrite").parquet(f"{VOLUME_PATH}/taxi_parquet")
+
+# Lookup-Daten als Parquet speichern
+df_lookup.write.mode("overwrite").parquet(f"{VOLUME_PATH}/lookup_parquet")
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### 4.4.2. Auslesen
+
+# COMMAND ----------
+
+df_taxi_parquet = spark.read.parquet(f"{VOLUME_PATH}/taxi_parquet")
+df_lookup_parquet = spark.read.parquet(f"{VOLUME_PATH}/lookup_parquet")
+
+#df_taxi_parquet.show(5)
+display(df_taxi_parquet)
+
+df_lookup_parquet.show(5)
+
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##4.5. Speichern im Delta Format
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ###4.5.1. Speichern
 
 # COMMAND ----------
 
@@ -118,7 +153,7 @@ df_lookup.write.format("delta").mode("overwrite").save(f"{VOLUME_PATH}/lookup_de
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ###4.4.2. Auslesen
+# MAGIC ###4.5.2. Auslesen
 
 # COMMAND ----------
 
@@ -132,32 +167,30 @@ df_lookup_delta.show(5)
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ##4.5. Speichern im JSON Format
+# MAGIC ##4.4.3 Time Travel
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ### 4.5.1 Speichern
+from pyspark.sql.functions import col
 
-# COMMAND ----------
-
-# Taxi-Daten als JSON speichern
-df_taxi.write.mode("overwrite").json(f"{VOLUME_PATH}/taxi_json")
-
-# Lookup-Daten als JSON speichern
-df_lookup.write.mode("overwrite").json(f"{VOLUME_PATH}/lookup_json")
+df_taxi_delta_km = df_taxi_delta.withColumn("trip_distance", col("trip_distance") * 1.60934)
 
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ###4.5.2. Einlesen
+df_taxi_delta_km.write.format("delta").mode("overwrite").save(f"{VOLUME_PATH}/taxi_delta")
+
 
 # COMMAND ----------
 
-df_taxi_json = spark.read.json(f"{VOLUME_PATH}/taxi_json")
-df_lookup_json = spark.read.json(f"{VOLUME_PATH}/lookup_json")
+display(spark.sql(f"DESCRIBE HISTORY delta.`{VOLUME_PATH}/taxi_delta`"))
 
-df_taxi_json.show(5)
-df_lookup_json.show(5)
+df_taxi_delta_0 = spark.read.format("delta").option("versionAsOf", 0).load(f"{VOLUME_PATH}/taxi_delta")
+display(df_taxi_delta_0)
 
+df_taxi_delta_1 = spark.read.format("delta").option("versionAsOf", 1).load(f"{VOLUME_PATH}/taxi_delta")
+display(df_taxi_delta_1)
+
+#Frage: Was passiert, wenn ich ohne versionAsOf Lade?
+
+#ToDO: Laden über timestampAsOf
